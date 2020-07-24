8e47ccfc0937f87ced1060bf004082c77cc27162
sbvhfdcw <15914381551@163.com>
2020-07-24 10:51:09 +0800

    ccc

--- sp02-itemservice/src/main/java/com/tedu/sp02/Sp02ItemserviceApplication.java
+++ sp02-itemservice/src/main/java/com/tedu/sp02/Sp02ItemserviceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class Sp02ItemserviceApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(Sp02ItemserviceApplication.class, args);
 	}
